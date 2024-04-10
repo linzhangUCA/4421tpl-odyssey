a30206850d9090adf1de944150e6a710b81bc6ec
linzhanguca <lzhang12@ucae.edu>
2024-04-10 09:52:26 -0500

    add urdf

--- diffbot_navigation/setup.py
+++ diffbot_navigation/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'diffbot_navigation'
@@ -10,11 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
+        (os.path.join('share', package_name, 'world'), glob(os.path.join('world', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='pbd0',
-    maintainer_email='lzhang12@ucae.edu',
+    maintainer='TODO: Maintainer name',
+    maintainer_email='TODO: Maintainer email',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
